3fd89c4ba9855c811f5149e6c79ea0e69cc393d0
Hugo Peres <hugo-pdf@hotmail.com>
2019-04-17 13:47:50 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/takt', (req, res) => res.send('Hello Takt!'));
+
+module.exports = app;
