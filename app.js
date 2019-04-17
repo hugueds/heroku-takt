7715edac86b2c39b8f0a50624d825a4fd5c2014c
Hugo Peres <hugo-pdf@hotmail.com>
2019-04-17 16:02:16 -0300

    update

--- app.js
+++ app.js
@@ -6,17 +6,13 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-const io = require('socket.io');
-
-
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.sendFile('./index.html'));
 app.get('/takt', (req, res) => res.send('Hello Takt!'));
 
 module.exports = app;
