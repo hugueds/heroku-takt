89558128950a4372cfa820fc300bc2b06b6561f0
Hugo Peres <hugo-pdf@hotmail.com>
2019-04-17 16:06:21 -0300

    update

---
+++ clientSocket.js
@@ -0,0 +1,21 @@
+const io = require('socket.io-client');
+const HttpsProxyAgent = require('https-proxy-agent');
+
+let proxy = 'http://148.148.192.2:8080';
+
+const socket = io.connect('https://webpopids.herokuapp.com', {
+    agent: new HttpsProxyAgent(proxy)
+});
+
+
+let counter = 0;
+socket.on('connect', () => {
+    console.log('ok')
+})
+
+setInterval(() => {
+    counter++;
+    socket.emit('counter', counter);
+})
+
+
